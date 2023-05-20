70d7caa1f442bec754a99a6ee351fd8e4beef31f
Babur1992 <jon1992@ukr.net>
2023-05-20 16:09:45 +0300

    ДЗ_9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,14 +2,15 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 
-function promiceDelay(delay) {
+function delayPromise(delay) {
   return new Promise(resolve => {
     setTimeout(resolve, delay);
   });
 }
 
-function createPromise(position, delay) {
+ function createPromise(position, delay) {
   try {
+    // await delayPromise(delay);
     const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
       return { position, delay };
@@ -20,12 +21,11 @@ function createPromise(position, delay) {
     throw error;
   }
 }
-
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  const delay = Number(evt.target.elements.delay.value);
-  const step = Number(evt.target.elements.step.value);
-  const amount = Number(evt.target.elements.amount.value);
+form.addEventListener('submit',  event => {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
   try {
     for (let i = 1; i <= amount; i++) {
       const { delay: promiseDelay } = createPromise(i, delay);
@@ -38,3 +38,43 @@ form.addEventListener('submit', evt => {
     Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
   }
 });
+
+
+// const form = document.querySelector('.form');
+
+// function promiceDelay(delay) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, delay);
+//   });
+// }
+
+// function createPromise(position, delay) {
+//   try {
+//     const shouldResolve = Math.random() > 0.3;
+//     if (shouldResolve) {
+//       return { position, delay };
+//     } else {
+//       throw { position, delay };
+//     }
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+// form.addEventListener('submit', evt => {
+//   evt.preventDefault();
+//   const delay = Number(evt.target.elements.delay.value);
+//   const step = Number(evt.target.elements.step.value);
+//   const amount = Number(evt.target.elements.amount.value);
+//   try {
+//     for (let i = 1; i <= amount; i++) {
+//       const { delay: promiseDelay } = createPromise(i, delay);
+//       console.log(`Fulfilled promise ${i} in ${promiseDelay}ms`);
+//       Notiflix.Notify.success(`Fulfilled promise ${i} in ${promiseDelay}ms`);
+//     }
+//   } catch (error) {
+//     const { position, delay } = error;
+//     console.log(`Rejected promise ${position} in ${delay}ms`);
+//     Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+//   }
+// });
